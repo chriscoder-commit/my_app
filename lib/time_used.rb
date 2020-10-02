ba272b9d2a26a3ecb8aea74db8c28e7271d34931
Chris Scott <chris.pb.scott@gmail.com>
2020-10-01 19:07:09 -0700

    time waster function now substantially complete

--- lib/time_used.rb
+++ lib/time_used.rb
@@ -1,18 +1,13 @@
 class TimeUsed
   attr_reader :weekly_playtime, :yearly_playtime
 
-  def initialize
-    @weekly_playtime = weekly_playtime
-    @yearly_playtime = yearly_playtime
-  end 
-
   def user_input
     puts "Do you often play video-games? (yes/no)"
     print '> '
-    frequency = gets.chomp
+    frequency = gets.chomp.downcase
     if frequency == "yes"
       puts "How many hours a week on average do you think you have played video-games in the past month?"
-      @weekly_playtime = gets.chomp.to_i
+      @weekly_playtime = gets.chomp.to_f
     elsif frequency == "no"
       puts "Are you using a stick to press the keys while you're up on your high horse!?"
       exit 
@@ -21,41 +16,52 @@ class TimeUsed
     end 
   end
 
+  def skills 
+    case @yearly_playtime 
+    when 1..30
+      puts "Did you know you could have learned to drive manual?"
+    when 31..100
+      puts "also working"
+    when 101..500
+      french = 400 - @yearly_playtime
+      extra_time = french.abs 
+      if @yearly_playtime < 400
+        puts "Did you know it would take you around 400 hours to learn French? So in around another #{french} hours, you could have been annoying friends and family!"
+      else @yearly_playtime > 400
+        puts "You could have already learned French with time to spare! Approximately #{extra_time} hours to spare!"
+      end 
+    when 501..1500
+      guitar = 900 - @yearly_playtime
+      extra_time = guitar.abs
+      puts "Did you know it would take around 900 hours to be able to play any song on guitar? You could even improvise!"
+      if @yearly_playtime < 900
+        puts "It would only take you another #{guitar} hours to learn this!"
+      else @yearly_playtime > 900
+        puts "You could have already learned guitar, with time to spare! You could have been serenading the world for the last #{extra_time} hours!"
+      end 
+    when 1501..8760
+      puts "Same honestly, but I think we oughta go for a jog or something?"
+    when (8761..)
+      puts "There aren't that many hours in a year, stop being silly!"
+      return 
+    end 
+  end 
+
   def time_wasted
     user_input
-    yearly_playtime = ((@weekly_playtime * 4) * 12)
-    puts "Oh wow! So assuming you have sustained this average for the past year, you'd have played for #{yearly_playtime} hours!"
+    @yearly_playtime = ((@weekly_playtime * 4) * 12)
+    puts "Oh wow! So assuming you have sustained this average for the past year, you'd have played for somewhere around #{@yearly_playtime} hours!"
     puts "Are you interested in knowing what else you could have done with this time? (yes/no)"
     print '> '
-    interest = gets.chomp
+    interest = gets.chomp.downcase 
     if interest == "yes"
-      puts "Okay!"
-      calculation = yearly_playtime
-      case yearly_playtime 
-      when yearly_playtime = 1..30
-        puts "working"
-      when yearly_playtime = 31..100
-        puts "also working"
-      when yearly_playtime = 101..500
-        french = 400 - calculation
-        puts "Did you know it would take you around 400 hours to learn French? So in around another #{french} hours, you could have been annoying friends and family!"
-      when yearly_playtime = 501..1000
-        puts "thanks for working"
-      end 
+      skills 
     elsif interest == "no"
       puts "Possibly a wise decision!"
       puts "Returning to main menu"
       return
-    begin
+    else 
       puts "Error! Try again!"
-      return 
-    rescue => exception
-      
-    else
-      
-    ensure
-      
-    end
     end 
   end 
   
